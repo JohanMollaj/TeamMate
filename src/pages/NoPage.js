d2f106d1a7fb6759ee2dfbf0bcd46d2c6bae0fef
Johan <johanmollaj12@gmail.com>
2025-02-19 19:35:30 +0100

    tasks wip

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -3,7 +3,7 @@
 export default function(){
     return(
         <div>
-            <h1>404 : No Page Found</h1>
+            <h1 className='px-20 py-50 text-white' >404 : No Page Found</h1>
         </div>
     )
 }
--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -1,9 +1,28 @@
-
+import './tasks.css';
+import React, { useState, useEffect } from 'react';
 
 export default function(){
     return(
         <div>
-            
+            <div className="container-tasks">
+                {/* Dashboard */}
+                <div className="wrapper tasks">
+                <div  className='TasksHeader'>
+                            <h1>Tasks</h1>
+                        </div>
+                    <div className='sections'>
+                        
+                        <div className="section overview">
+                            <h2>Overview</h2>
+                        </div>
+
+                        <div className="section assignments">
+                            <h2>Assignments</h2>
+                            <p>You have finished all your tasks!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
